70e8ac810fb9ae1c7ed9b7091e351be9abc1ce12
Usmanahmedintern <usman_ahmed_intern@netixsol.com>
2023-12-19 00:34:22 +0500

    images issue

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 import { RxCross2 } from "react-icons/rx";
 import useJobListingStore from "../store/JobListingStore"
 import ToggleSwitch from "./ToggleSwitch";
+import LogoDesktop from "../assets/images/bg-header-desktop.svg"
+import LogoMobile from "../assets/images/bg-header-mobile.svg"
 
 const Header = () => {
     const { removeTag, selectedTags, clearAllFilters } = useJobListingStore((state) => ({
@@ -12,8 +14,8 @@ const Header = () => {
     return (
         <div>
             <div className="flex justify-center items-center shadow-sm bg-[#5ea5a3]">
-                <img src="/images/bg-header-desktop.svg" alt="header-Desktop" className="md:block hidden" />
-                <img src="/images/bg-header-mobile.svg" alt="header-mobile" className="w-full block md:hidden " />
+                <img src={LogoDesktop} alt="header-Desktop" className="md:block hidden" />
+                <img src={LogoMobile} alt="header-mobile" className="w-full block md:hidden " />
                 <ToggleSwitch className="absolute right-3"/>
             </div>
             {
